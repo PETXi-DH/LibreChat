4365e111f9c2d5a825a1ea9316eade28eb156128
Dan <dhunt@pet-xi.co.uk>
2024-10-28 22:00:18 +0000

    28.10.24 - Reduced UI

--- client/src/components/Chat/Footer.tsx
+++ client/src/components/Chat/Footer.tsx
@@ -39,7 +39,7 @@ export default function Footer({ className }: { className?: string }) {
       ? config.customFooter
       : '[LibreChat ' +
         Constants.VERSION +
-        '](https://librechat.ai) - ' +
+        ']() - ' +
         localize('com_ui_latest_footer')
   ).split('|');
 
